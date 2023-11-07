08dc28ba1a7676e8750362f95f1e06e1af6cea61
ElygledsonJs <elygledsonjs@gmail.com>
2023-11-07 17:23:31 -0300

    CR fixes

--- question_generators/mcq.py
+++ question_generators/mcq.py
@@ -1,15 +1,15 @@
-import os
-import re
 import json
 
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
+import openai
+import json
+
 from config.environment import config
 
-os.environ["OPENAI_API_KEY"] = config.get('OPENAI_API_KEY')
-
+openai.api_key = config.get('OPENAI_API_KEY')
 
 class MultipleChoiceQuestion:
     def __init__(self):
@@ -35,6 +35,47 @@ class MultipleChoiceQuestion:
                                     partial_variables={"format_instructions": format_instructions})
         user_query = prompt.format_prompt(user_prompt=text, limit=num)
         user_query_output = self.llm(user_query.to_messages())
-        json_string = re.search(r'```json\n(.*?)```',
-                                user_query_output.content, re.DOTALL).group(1)
-        return json.loads(f'[{json_string}]')
+        print(type(user_query_output.content), )
+        content = user_query_output.content.replace('```json', '')
+        content = content.replace('}','},')
+        content = content.replace(',\n```', '')
+        return json.loads(f'[{content}]')
+    
+    def generate_using_openai(self, content, limit):
+        mcq_function = [
+        {
+            "name": "create_mcq",
+            "description": f"Generate {limit} multiple choice questions from the input text with four candidate options. Three options are incorrect and one is correct. Indicate the correct option after each question.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "questions": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "question":{"type": "string", 
+                                            "description": "A multiple choice question extracted from the input text."},
+                                 
+                                "options":{"type": "array",
+                                           "items":{"type":"string",
+                                                    "description": "Candidate option for the extracted multiple choice question."} 
+                                            },
+                                 
+                                "answer":{"type": "string", 
+                                          "description": "Correct option for the multiple choice question."}
+                            }                            
+                          }
+                        }
+                    }
+                },
+                "required": ["questions"]
+            }
+        ]
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": content}],
+            functions=mcq_function,
+            function_call={"name": "create_mcq"},
+        )
+        return json.loads(response['choices'][0]['message']['function_call']['arguments'])
--- routes/question.py
+++ routes/question.py
@@ -1,40 +1,42 @@
 
-from models.request import Question, QuestionType, Request
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
 from models.request import Request
+from fastapi.responses import JSONResponse
+from models.request import  QuestionType, Request
 from question_generators.mcq import MultipleChoiceQuestion
 from question_generators.speech_to_text import SpeechToText
 
 router = APIRouter()
 
 
+# @router.post('/text/questions')
+# def generate_from_text(request: Request):
+
+
+
+# @router.post('/transcription/questions')
+# def generate_questions_from_url(request: Request):
+#     url = request.content
+#     num = request.num
+#     question_type = request.type
+#     transcription = SpeechToText.get_transcription_from_youtube(url)
+#     return handle(question_type, transcription, num)
+
+
 @router.post('/text/questions')
-def generate(request: Request):
-    text = request.content
-    num = request.num
-    question_type = request.type
-    return handle(question_type, text, num)
+def generate_questions_from_text(request: Request):
+      content = request.content
+      num = request.num
+      question_type = request.type
+      return handle(question_type, content, num)
 
-
-@router.post('/transcription/questions')
-def generate_questions_from_url(request: Request):
-    url = request.content
-    num = request.num
-    question_type = request.type
-    transcription = SpeechToText.get_transcription_from_youtube(url)
-    return handle(question_type, transcription, num)
-
-
-def handle(question_type, text, num):
+def handle(question_type, content, num):
     generated_questions = []
     try:
         if question_type == QuestionType.multiple_choice_question:
             mcq_questions = MultipleChoiceQuestion()
-            generated_questions = mcq_questions.generate(
-                text, num)
-            generated_questions = [{ 'description': question['description'], 'options': question['options'].split('\n'), 'answer': question['answer'] } for question in generated_questions]
-        return JSONResponse(content={'questions': generated_questions, "message": "Questões geradas com sucesso!"})
-    except Exception as e:
-        print(e)
+            print(num)
+            generated_questions = mcq_questions.generate_using_openai(content, num)
+        return JSONResponse(content={'questions': generated_questions['questions'], "message": "Questões geradas com sucesso!"})
+    except:
         return JSONResponse(content={'message': 'Erro ao gerar as questões.'}, status_code=500)
